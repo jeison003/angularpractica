211ee474b9069d6a3174ebd4c2c7b55744f55d17
jeison003 <jeisonmartinezev@unimagdalena.edu.co>
2023-06-15 15:32:22 -0500

    ajustes con comentatios

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -14,6 +14,7 @@ export class MainPagesComponent {
 
   }
   get characters(): Character[]{
+    // los [...data] hace referencia a que crea una copia de los character, asi si se modifican no afectana los originales
     return [...this.DbzService.characters];
   }
 
